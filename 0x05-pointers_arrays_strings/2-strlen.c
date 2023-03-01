4e117a07b592e5a7a6370f76e0d628fcb40ebc1f
Folacodee <folasmith950@gmail.com>
2023-02-28 19:32:29 -0800

    str

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,14 @@
 
 /**
  * _strlen - return the length of a string
- * @str: the string to get the length of 
+ * @str: the string to get the length of
  * Return: the length of @str.
  */
 size_t _strlen(const char *str)
-
 {
 	size_t length = 0;
 
 	while (*str++)
 	length++;
 	return (length);
-
 }
